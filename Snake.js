731170d322f66e18c47f4bac70b0fd6e2b4dbe1d
JuniorDevPort <84349059+JuniorDevPort@users.noreply.github.com>
2021-09-07 19:45:04 +0300

    See on uuem versioon

--- Snake.js
+++ Snake.js
@@ -13,13 +13,13 @@ this.dir = function(x, y) {
 }
 
     this.update = function() {
-this.x = this.x + this.xspeed*scl; 
-this.y = this.y + this.yspeed*scl;
+this.x = this.x + this.xspeed; 
+this.y = this.y + this.yspeed;
 }
 
 this.show = function() {
     fill(255);
-    rect(this.x, this.y, scl, scl);
+    rect(this.x, this.y, 10, 10);
 
 }
 }
