401a5a4e4f1aa0f2ef0e7042a82f265f8d3cd468
JuniorDevPort <84349059+JuniorDevPort@users.noreply.github.com>
2021-09-08 14:50:37 +0300

    asdasdasdasd

---
+++ PromtSimple.js
@@ -0,0 +1,23 @@
+/*
+
+take user input
+make a field box
+so i see an input box with the the text type your name
+when you have the name print it out with the text > i greet you, sir
+g
+*/
+
+var name = window. prompt("Enter your name: ");
+
+if (name == ('Bob')) {
+    alert('> i greet you, sir ' + name);
+
+}else if (name == ('Alice')) {
+    alert('> i greet you, sir ' + name);
+
+}else {
+
+alert('Not in database')
+
+}
+
