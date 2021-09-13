359a346788a286033066c5ee860b076a66d4c0f4
JuniorDevPort <84349059+JuniorDevPort@users.noreply.github.com>
2021-09-13 16:12:59 +0300

    See on 4;12

---
+++ TableExcercise.js
@@ -0,0 +1,29 @@
+/* 
+
+for(number of index)
+ print the row of elements
+
+ */
+ var m = 5;
+ let list1 = [];
+ let list2 = [];
+ for(let j = 1; j<=m; j++){
+   let list1 = [];
+   for(let i = 1; i<=m; i++){
+   
+   
+   list1.push(i);
+  //list2.push(j);
+ 
+  //, list2);
+   
+
+}
+console.log(list1);
+ }
+
+/* 
+now i have the upper side of the table
+now create the one for the down side 
+
+*/
