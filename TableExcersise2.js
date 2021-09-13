acb2cc3b88d078a10b3723393b87e0b78e7c5beb
JuniorDevPort <84349059+JuniorDevPort@users.noreply.github.com>
2021-09-13 21:58:17 +0300

    New logic for table

--- TableExcersise2.js
+++ TableExcersise2.js
@@ -1,28 +1,32 @@
 
 
 
-list1 = [];
+list1 = [1], [2], [3];
 list2 = [];
 var b;
 var n = 1;
 var m = 1;//kui ei onnestu alusta nullist ja tee exception
 
-for(i=1; i<=3; i++){
+for(i=1; i<=2; i++){
     list1.push(n);
     n++;
-
-   
-
+    
 }
 
 console.log(list1);
 
-for(i=1; i<=5; i++){
-    list2.push(m);
+list2.push(m);
+
+for(i=1; i<=2; i++){
+    
+    console.log(list2);
     m++;
-
+    list2[0] = m
    
-
 }
-console.log(list2);
+
+//for loop, mis kaib labi yhe array ja korrutab selle teise arrayga
+
+
+
 
