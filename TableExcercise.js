6f46fea479b32643021c6c32a0d0bcc66a2d571d
JuniorDevPort <84349059+JuniorDevPort@users.noreply.github.com>
2021-09-13 21:35:12 +0300

    Seeon tablatuur

--- TableExcercise.js
+++ TableExcercise.js
@@ -1,29 +1,10 @@
-/* 
+//print a row of values
 
-for(number of index)
- print the row of elements
+list1 = [];
 
- */
- var m = 5;
- let list1 = [];
- let list2 = [];
- for(let j = 1; j<=m; j++){
-   let list1 = [];
-   for(let i = 1; i<=m; i++){
-   
-   
+for(i=1; i<=5; i++){
    list1.push(i);
-  //list2.push(j);
- 
-  //, list2);
-   
+
+   console.log(list1);
 
 }
-console.log(list1);
- }
-
-/* 
-now i have the upper side of the table
-now create the one for the down side 
-
-*/
---
+++ TableExcersise2.js
@@ -0,0 +1,28 @@
+
+
+
+list1 = [];
+list2 = [];
+var b;
+var n = 1;
+var m = 1;//kui ei onnestu alusta nullist ja tee exception
+
+for(i=1; i<=3; i++){
+    list1.push(n);
+    n++;
+
+   
+
+}
+
+console.log(list1);
+
+for(i=1; i<=5; i++){
+    list2.push(m);
+    m++;
+
+   
+
+}
+console.log(list2);
+
