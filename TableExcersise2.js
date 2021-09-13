67172f2846218f802528ce95e8f9fc27066d2574
JuniorDevPort <84349059+JuniorDevPort@users.noreply.github.com>
2021-09-13 22:10:47 +0300

    two variable based logic

--- TableExcersise2.js
+++ TableExcersise2.js
@@ -2,6 +2,66 @@
 
 
 list1 = [1], [2], [3];
+
+for every list element multiply by every list element
+
+n = 0;\
+
+m = 1;
+
+
+for length of array multiply every element with other element
+
+list1[n] = list1[n] * m
+n++;
+
+
+next iteration starting value for m is +1;
+
+m++
+(m = 1)
+
+list1[n] = list1[n] * m
+n++;
+
+
+
+wait, one value indexes the number of the index in array, the other represents the multiplying factor.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 list2 = [];
 var b;
 var n = 1;
