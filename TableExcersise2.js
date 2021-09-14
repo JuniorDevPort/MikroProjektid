5fc25b7e4b4d91aaf41ecaf9b2b399b9e9a415ab
JuniorDevPort <84349059+JuniorDevPort@users.noreply.github.com>
2021-09-14 17:41:53 +0300

    Last and faulty version, array conceptual roblem

--- TableExcersise2.js
+++ TableExcersise2.js
@@ -1,83 +1,80 @@
 
-/* 
-var list1 = [1, 2, 3, 4];
-
-//console.log(list1);
-
-
-list1[0] = list1[0] * list1[0];
-list1[1] = list1[0] * list1[1];
-list1[2] = list1[0] * list1[2];
-
-//console.log(list1);
-
-m = 6;
-n = 1;
-b = 0;//array index multiplier
-
-for(i=1; i<=m; i++){
-    var list1 = [1, 2, 3, 4];
-    
-console.log(list1);
-n++;
-}
-
-*/
-
-
-/* 
-//create a for loop that puthe the number of elements inside the Array
-m= 10; //number of elements in the table
+m= 3; 
 n= 1;
-b= 0; 
-var list1 = [];
 
-// this allows for the dynamic creation of a table
+
+var list1 = [];
+const list2 = [];
+
+//here i create the array with the m variable
 for(i=1; i <= m; i++){
     
     list1.push(n);
-    n++;
-    //  console.log(list1);
+    list2
+.push(n);
+    n++; 
 
 }
 
-  //  b peab olema samasuur voi vordne elementide arvuga. sest b on see mida ma suurendan 1 vorra selleks, et 
 
-//for the number of elements
-*/
-var list1 = [1, 2, 3, 4];
+//console.log(list2, list1);
 
-var a = list1;
-
-//m= 3; 
 n= 1;
 b= 0; 
 
-for(j=1; j <= list1.length ; j++){
+//for(j=1; j <= list1.length ; j++){
+    b= 0;
+    list1 = list2
+;
 
-    console.log("--------------------Esimene loop--------------------");
+    //console.log("--------------------Esimene loop--------------------");
+    //console.log("seeon list2= ------------",list2);
 
-for(i=1; i <= list1.length ; i++){
+//for(i=1; i <= list1.length ; i++){
     list1[b] = list1[b] * n;
 
 
-    console.log(list1);
-    console.log("index =",b ,"index value =", list1[b], "multiplied =", n  );
+    //console.log(list1);
+    //console.log("index =",b ,"index value =", list1[b], "multiplied =", n  );
 
-
-    //n++;
     b++;
 
-}
+//n++;
+console.log("see on LIST222" ,list2);
+
+console.log(list2
+, list1);
+
+//-------
+
+    list1[b] = list1[b] * n;
+    
+    b++;
+
+
+
+console.log(list2
+, list1);
+console.log("see on LIST222" ,list2);
+//-----------
+list1[b] = list1[b] * n;
+    
+b = 0;
+
 n++;
-b= 0;
-//console.log(a);
-list1 = [1, 2, 3, 4];
-}
+
+//------------------------------------------------------------------------------
+console.log(list2, list1);
+console.log("see on LIST222" ,list2);
+console.log(list2[0]);
+list1[b] = list1[b] * n;
+console.log(list2[0]);
+console.log("see on LIST222" ,list2);   
+b++;
 
 
 
-
+console.log("see on LIST222" ,list2,"See on LIST 1", list1);
 
 
 
