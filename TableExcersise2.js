0077aa52a99709173158fe4849dbaa29f8b7c76b
JuniorDevPort <84349059+JuniorDevPort@users.noreply.github.com>
2021-09-15 00:16:16 +0300

    A new, fresh start, using trying to implement TDD elements

--- TableExcersise2.js
+++ TableExcersise2.js
@@ -1,82 +1,32 @@
 
-m= 3; 
-n= 1;
-
-
+var m = 3;
+var n = 1;
 var list1 = [];
-const list2 = [];
 
-//here i create the array with the m variable
-for(i=1; i <= m; i++){
+for(i=1; i <= m; i += 1){
     
     list1.push(n);
-    list2
-.push(n);
-    n++; 
+    n++;
+    
 
 }
 
+console.log(list1);
+console.assert(list1.length === m);
+console.assert(false,list1.length === m, "condition : OKAY");
 
-//console.log(list2, list1);
-
-n= 1;
-b= 0; 
-
-//for(j=1; j <= list1.length ; j++){
-    b= 0;
-    list1 = list2
-;
-
-    //console.log("--------------------Esimene loop--------------------");
-    //console.log("seeon list2= ------------",list2);
-
-//for(i=1; i <= list1.length ; i++){
-    list1[b] = list1[b] * n;
+/* 
+3 testi:
 
 
-    //console.log(list1);
-    //console.log("index =",b ,"index value =", list1[b], "multiplied =", n  );
+assert array numbers are climbing +1 fashion
 
-    b++;
+assert it is a type of array
 
-//n++;
-console.log("see on LIST222" ,list2);
+*/
 
-console.log(list2
-, list1);
+assert that the table elemnts are the result of multiplying the list by the indexedDB.
 
-//-------
+for loop : loop through the list and multiply by index : assert that each elemtn is the result of multiplying the array elemetn with the index
 
-    list1[b] = list1[b] * n;
-    
-    b++;
-
-
-
-console.log(list2
-, list1);
-console.log("see on LIST222" ,list2);
-//-----------
-list1[b] = list1[b] * n;
-    
-b = 0;
-
-n++;
-
-//------------------------------------------------------------------------------
-console.log(list2, list1);
-console.log("see on LIST222" ,list2);
-console.log(list2[0]);
-list1[b] = list1[b] * n;
-console.log(list2[0]);
-console.log("see on LIST222" ,list2);   
-b++;
-
-
-
-console.log("see on LIST222" ,list2,"See on LIST 1", list1);
-
-
-
-
-
+for(i = 1; i <= array.length; i+=1)
