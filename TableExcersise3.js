bdb4bc529de74fbab1eb7b868350bb963aed5508
JuniorDevPort <84349059+JuniorDevPort@users.noreply.github.com>
2021-09-17 18:34:59 +0300

    Final, could use some pepping up.

--- TableExcersise3.js
+++ TableExcersise3.js
@@ -1,32 +1,46 @@
-var m = 3;
+var m = 9;
 var n = 1;
 
 var o = 0;
-var IndexOfTableArray = 1;
-var IndexOfTableArray2 = 1;
+var Index1 = 0;
+var Index2 = 2;
 
 var list1 = [];
 var list2 = [];
-var list3 = [];
 
 
-    
-   
+console.log(typeof list1);
+
+for(i=1; i<=m; i+= 1){
+  list1.push(n);
+
+  list2.push(n);
+  n++;
+
+}
+
+console.log(0, list2);
+
+
+n = 1;
+o = 0;
+
+
+for(u = 1; u <= m; u++){
+
+
+for(j=1; j<=m; j++){
+  list1[Index1] = list2[Index1] *  n;
+  
+  Index1++;
+  
+}
+//list2.splice(0, 0, Index1);
+console.log(n, list1);
+Index1 = 0;
+n++;
+}
 
 
 
-console.log(list1);
 
-
-
-
-
-
-
-
-
-for(j=1; j <= list1.length; j +=1){
-    
-        
-        for(i=1; i <= list1.length; i +=1){
-            list1[o] = list1[o] + IndexOfTableArray*2
